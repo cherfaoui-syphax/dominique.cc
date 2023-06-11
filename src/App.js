c9c7d7e960dd357a7591b79110c67242c88f0971
cherfaoui-syphax <ks_cherfaoui@esi.dz>
2023-06-11 15:31:12 +0100

    film making page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import HomepPage from './components/homePage';
 import ContactPage from './components/contactsPage';
-
+import FilmMaking from "./components/filmMaking";
 import React from 'react';
 import { Routes , Route, Link } from 'react-router-dom';
 
@@ -16,6 +16,7 @@ function App() {
       <Routes >
         <Route exact path="/" element={<HomepPage/>} />
         <Route path="/contact" element={<ContactPage/>} />
+        <Route path = "/film-making" element = {<FilmMaking/>} ></Route>
       </Routes >
     </div>
   );
---
+++ src/components/filmMaking.jsx
@@ -0,0 +1,77 @@
+import ReactDOM from 'react-dom/client';
+import React, { useEffect } from 'react';
+import { Grid  , Box , Paper} from '@mui/material';
+import LeftImages from './leftImages';
+import '../App.css';
+import MainColumn from './mainColumn';
+const containerStyle={
+    width : "90%",
+    height : "fit-content",
+    margin : "auto",
+    padding:"5%"
+}
+
+
+export default function FilmMaking() {
+    useEffect(() => {
+        setTimeout(() => {
+          document.querySelector('#background').classList.add('visible');
+          document.querySelector('#pixel-image').classList.add('visible');
+        }, 100);
+      }, []);
+    
+  return (
+    <div id="home">
+        <div id = "background">
+        </div>
+        <Box style={{
+                position: "fixed",
+                width: "20%",
+                height:"100%",
+                overflow: "hidden",
+                top:0,
+                right:0
+            }}
+        sx= {{display : { xs: 'none', md: 'block' }}}>
+            <img style={{
+                width: "100%",
+                height:"100%",
+            }} src={require("../assets/cityscape.gif")}></img>
+        </Box>
+
+        <Grid container justifyContent="flex-end">
+            <Grid item xs={12} md={9} >
+                <img style={{
+                    width : "50%",
+                    marginLeft :"50%"
+
+                }}
+                src={require("../assets/title.png")}></img>
+
+                <div className='parent-div ' style={{
+                    width : "50%",
+                    height : "5vh",
+                    marginLeft :"50%",
+                    objectFit: "cover",
+                    overflow: "hidden",
+                }}><img id="pixel-image" src={require("../assets/multipixels.jpg")}></img></div>
+                
+                <Grid container justifyContent="center">
+                    <Grid item xs={12} md={3}>
+                        <LeftImages/>
+                    </Grid>
+                    <Grid item xs={12} md={9}>
+                        <Paper style = {containerStyle} elevation={3}>
+
+                            <div style = {{ minHeight : 1000 , width : "100%" , backgroundcolor: "#aaaaaa"  }}></div>
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={0} md={3}>
+            </Grid>
+        </Grid>
+
+    </div>
+  );
+}
